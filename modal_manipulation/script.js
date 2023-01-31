4925f62814a325acfddadccf74c4b6e8e3e8277b
Simon Love <simon-love@hotmail.co.uk>
2023-01-31 12:00:01 +0000

    Opening and closing modal with click & key events

--- modal_manipulation/script.js
+++ modal_manipulation/script.js
@@ -22,9 +22,13 @@ for (let i = 0; i < btnsOpenModalVar.length; i++) {
     btnsOpenModalVar[i].addEventListener("click", openModal);
 }
 
+//close modal with corner x
 btnCloseModalVar.addEventListener("click", closeModal);
+
+//close modal clicking on the overlay, anything not in the modal
 overlayVar.addEventListener("click", closeModal);
 
+//close modal using just the ESC key
 document.addEventListener("keydown", function (e) {
     // console.log(e.key);
     if (e.key === "Escape" && !modalVar.classList.contains("hidden")) {
